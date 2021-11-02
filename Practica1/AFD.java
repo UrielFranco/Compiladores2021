2bd858ce19a946cc1c0d846aee8c194ed689b1ab
UrielFranco <63439057+UrielFranco@users.noreply.github.com>
2021-11-01 19:31:16 -0600

    Add files via upload

---
+++ Practica1/AFD.java
@@ -0,0 +1,63 @@
+
+
+public class AFD {
+
+    
+    
+    String afabeto;                             //alfacbeto de entrada
+    String transicion;                          //funcion de transicion
+    int q0;                                     //estado inicial del automata
+    int[ ] estados = new int[40];               //conjunto finito de estados el automata
+    int[ ] estadosFin = new int[4];             //conjunto de estados finales
+    
+
+
+
+    public AFD(String afabeto, String transicion) {
+        this.afabeto = afabeto;
+        this.transicion = transicion;
+    }
+
+    public String getAfabeto() {
+        return afabeto;
+    }
+
+    public void setAfabeto(String afabeto) {
+        this.afabeto = afabeto;
+    }
+
+    public String getTransicion() {
+        return transicion;
+    }
+
+    public void setTransicion(String transicion) {
+        this.transicion = transicion;
+    }
+
+    public int getQ0() {
+        return q0;
+    }
+
+    public void setQ0(int q0) {
+        this.q0 = q0;
+    }
+
+    public int[] getEstados() {
+        return estados;
+    }
+
+    public void setEstados(int[] estados) {
+        this.estados = estados;
+    }
+
+    public int[] getEstadosFin() {
+        return estadosFin;
+    }
+
+    public void setEstadosFin(int[] estadosFin) {
+        this.estadosFin = estadosFin;
+    }
+    
+   
+    
+}
